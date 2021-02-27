e39c60bb9fdbaaa5f35676be940fc199aafd69cf
stutiid <stutichaturvedi12@gmail.com>
2021-02-27 11:17:21 +0530

    Added: Calculate salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -x
+
+#constants
+EMP_RATE_PER_HR=20
+FULL_TIME_HRS=8
+ISPRESENT=1
+
+#variables
 empCheck=$((RANDOM%2))
-if [ $empCheck -eq 0 ]
+
+if [ $empCheck -eq $ISPRESENT ]
 then
-	echo absent
+	salary=$(($EMP_RATE_PER_HR*$FULL_TIME_HRS))
+	echo $salary
 else
-	echo present
+	echo "Employee is absent"
 fi
 
 
