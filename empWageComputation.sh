23cfa9cba10e1716887704b0328bc2db6440f1cc
stutiid <stutichaturvedi12@gmail.com>
2021-02-27 20:45:50 +0530

    Added: Employee wage monthly

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,28 @@
 #!/bin/bash -x
 
-empCheck=$((RANDOM%3))
-isFullTime=1
-isPartTime=2
-#isPresent=1
-empRatePerHr=20;
-
-case $empCheck in
-   $isFullTime)
-      empHrs=8
-   ;;
-   $isPartTime)
-      empHrs=4
-   ;;
-   *)
-      empHrs=0
-   ;;
-esac
-salary=$(($empHrs*$empRatePerHr))
-echo $salary
+#constants are written in Captital letters only
+IS_FULLTIME=1
+IS_PARTTIME=2
+EMP_RATE_PER_HR=20;
+NUMBER_OF_WORKING_DAYS=20
+#variable
+totalSalary=0
+for ((day=1; day<=$NUMBER_OF_WORKING_DAYS; day++))
+do
+   empCheck=$((RANDOM%3))
+   case $empCheck in
+      $IS_FULLTIME)
+         empHrs=8
+      ;;
+      $IS_PARTTIME)
+         empHrs=4
+      ;;
+      *)
+         empHrs=0
+      ;;
+   esac
+   salary=$(($empHrs*$EMP_RATE_PER_HR))
+   totalSalary=$(($totalSalary+$salary))
+done
+echo $totalSalary
 
